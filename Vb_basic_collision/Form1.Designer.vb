3b128d1f63d805b4022d2896dd0d5edfb1f79e17
alexjay1997 <aljayfauni@gmail.com>
2020-04-26 20:09:48 +0800

    initial commit

---
+++ Vb_basic_collision/Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.player = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.player, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 20
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Red
+        Me.PictureBox1.Location = New System.Drawing.Point(633, 294)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(53, 115)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Tag = "enemy"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Red
+        Me.PictureBox2.Location = New System.Drawing.Point(467, 82)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(55, 115)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        Me.PictureBox2.Tag = "enem"
+        '
+        'player
+        '
+        Me.player.Image = Global.Vb_basic_collision.My.Resources.Resources.giphy
+        Me.player.Location = New System.Drawing.Point(51, 149)
+        Me.player.Name = "player"
+        Me.player.Size = New System.Drawing.Size(127, 155)
+        Me.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.player.TabIndex = 0
+        Me.player.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.player)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.player, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents player As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+End Class
---
+++ Vb_basic_collision/Form1.vb
@@ -0,0 +1,110 @@
+﻿Public Class Form1
+    Dim goUp As Boolean = False
+    Dim goDown As Boolean = False
+    Dim goLeft As Boolean = False
+    Dim goRight As Boolean = False
+    Dim playerSpeed As Integer = 10
+
+    Private Sub Keydown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If (e.KeyCode = Keys.Up) Then
+            goUp = True
+
+        End If
+        If (e.KeyCode = Keys.Down) Then
+            goDown = True
+
+        End If
+        If (e.KeyCode = Keys.Left) Then
+            goLeft = True
+
+        End If
+        If (e.KeyCode = Keys.Right) Then
+            goRight = True
+
+        End If
+
+
+    End Sub
+
+    Private Sub Keyup(sender As Object, e As KeyEventArgs) Handles MyBase.KeyUp
+
+        goUp = False
+
+
+        goDown = False
+
+
+        goLeft = False
+
+
+        goRight = False
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+
+        If goUp = True Then
+            player.Top = player.Top - playerSpeed
+        End If
+
+        If goDown = True Then
+            player.Top = player.Top + playerSpeed
+        End If
+
+        If goLeft = True Then
+            player.Left = player.Left - playerSpeed
+
+        End If
+
+        If goRight = True Then
+            player.Left = player.Left + playerSpeed
+        End If
+
+        If Collision(player, PictureBox1) = True Then
+
+            PictureBox1.Dispose()
+        End If
+
+        If Collision(player, PictureBox2) = True Then
+
+            PictureBox2.Dispose()
+
+        End If
+
+
+    End Sub
+
+
+
+    Private Function Collision(ByVal Player1 As Object, ByVal Enemy1 As Object) As Boolean
+        Dim Collide As Boolean = False
+
+        If Player1.Top + Player1.Height >= Enemy1.Top And
+           Enemy1.Top + Enemy1.Height >= Player1.Top And
+            Player1.Left + Player1.Width >= Enemy1.Left And
+            Enemy1.Left + Enemy1.Width >= Player1.Left Then
+            Collide = True
+            Console.WriteLine(Collision)
+
+
+        End If
+
+        Return Collide
+
+
+
+    End Function
+
+    Private Sub player_Click(sender As Object, e As EventArgs) Handles player.Click
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
